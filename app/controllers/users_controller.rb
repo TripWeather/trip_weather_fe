54c79917e8f5534e3ddace5aaa1464699de19124
Erik Riggs <riggs.erik@gmail.com>
2022-11-02 21:58:29 -0600

    test: create user show_spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(session[:user_id])
   end
 
   # def new
---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Users Show" do
+  describe 'As a User' do
+    before :each do
+      @user = create(:user)
+      @user1 = create(:user)
+    end
+
+    it 'I see my username and my image' do
+      visit '/dashboard'
+
+      expect(page).to have_content()
+    end
+  end
