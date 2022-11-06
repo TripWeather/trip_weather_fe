d5c50199ea1b0aef59f254448140b64c2350f60a
Candace Eckels <candace.eckels@gmail.com>
2022-11-05 21:59:26 -0600

    Create trip service

--- app/services/trip_service.rb
+++ app/services/trip_service.rb
@@ -9,15 +9,31 @@ class TripService
     parse(response)
   end
 
-  def self.create_trip(name, uid, departure_date)
+  # def self.create_trip(name, uid, departure_date)
+  #   response = conn_post(name, uid, departure_date).get("#{uid}/trips")
+  #   parse(response)
+  # end
 
-  end
+  # def self.create_trip(name, uid, departure_date)
+  #   conn.post("/#{uid}/trips") do |req|
+  #    req.params[:name] = name
+  #    req.params[:departure_date] = departure_date
+  #   end
+  # end 
 
 private
 
   def self.conn
     Faraday.new('http://localhost:3000/api/v1/')
   end
+
+  # def self.conn_post(name, uid, departure_date)
+  #   Faraday.new("http://localhost:3000/api/v1/") do |req|
+  #     req.params['name'] = name,
+  #     req.params['departure_date'] = departure_date,
+  #     req.params['uid'] = uid
+  #   end
+  # end
 
   def self.parse(response)
     JSON.parse(response.body, symbolize_names: true)
